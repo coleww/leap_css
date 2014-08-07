edc39279d1ebb3743b68f933dde6cfdf73973a58
Cole Willsea <coleww@gmail.com>
2014-08-06 21:07:30 -0700

    proper indentation and stuff and cruft

--- leapcss.js
+++ leapcss.js
@@ -1,20 +1,23 @@
 (function() {
   function getCoord(pointer) {
-  return {
-    x: window.innerWidth / 2 + 3 * pointer.tipPosition[0],
-    y: window.innerHeight - pointer.tipPosition[1],
-    z: 180 + pointer.tipPosition[2],
-    id: pointer.id
-  };
-}
+    return {
+      x: window.innerWidth / 2 + 3 * pointer.tipPosition[0],
+      y: window.innerHeight - pointer.tipPosition[1],
+      z: 180 + pointer.tipPosition[2],
+      id: pointer.id
+    };
+  }
+  function randoColor(){
+    return "rgb(" + ~~(Math.random() * 255) + "," + ~~(Math.random() * 255) + "," +   ~~(Math.random() * 255)  +  ")";
+  }
   Leap.loop(function(frame) {
     for (var i = 0; i < frame.pointables.length && i < 1; i++) {
       var coord = getCoord(frame.pointables[i]);
-      el = document.elementFromPoint(coord.x, coord.y)
+      el = document.elementFromPoint(coord.x, coord.y);
       if (el){
-
-      el.style.backgroundColor = "rgb(" + ~~(Math.random() * 255) + "," + ~~(Math.random() * 255) + "," +   ~~(Math.random() * 255)  +  ")";
-    }
+        el.style.backgroundColor = "rgb(" + ~~(Math.random() * 255) + "," + ~~(Math.random() * 255) + "," +   ~~(Math.random() * 255)  +  ")";
+        el.style.color = randoColor();
+      }
     }
   });
 })();
